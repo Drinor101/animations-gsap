4d5242a0b88a2855ae6270caf1a1df82fe6b34ae
drinor101 <drinorshabiu1@gmail.com>
2025-07-07 15:32:03 +0200

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/gasp-animation/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/animations-gsap/' : '/',
 })
