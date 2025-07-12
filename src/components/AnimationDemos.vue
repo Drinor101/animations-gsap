275d639850a8df60b5fabcbb3b9110e0a20fb223
Drinor101 <69093680+Drinor101@users.noreply.github.com>
2025-07-13 00:06:01 +0200

    Fix SVG data URL encoding error

--- src/components/AnimationDemos.vue
+++ src/components/AnimationDemos.vue
@@ -134,7 +134,7 @@ const switchDemo = (demoId) => {
       <div class="rotating-element absolute bottom-1/4 right-1/4 w-12 h-12 border-2 border-purple-300/20 rounded-full"></div>
       
       <!-- Grid Pattern -->
-      <div class="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23ffffff" fill-opacity="0.03"%3E%3Ccircle cx="30" cy="30" r="1"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-50"></div>
+      <div class="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=%2260%22 height=%2260%22 viewBox=%220 0 60 60%22 xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cg fill=%22none%22 fill-rule=%22evenodd%22%3E%3Cg fill=%22%23ffffff%22 fill-opacity=%220.03%22%3E%3Ccircle cx=%2230%22 cy=%2230%22 r=%221%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-50"></div>
     </div>
 
     <div class="relative z-10 flex flex-col items-center justify-center min-h-screen p-4 md:p-8">
